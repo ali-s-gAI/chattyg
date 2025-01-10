578832fe7e95fd5abb06fd09a3ed1a84ec8464ad
ZA Syed <ali.syed@gauntletai.com>
2025-01-10 12:37:08 -0500

    delete dm loading page

--- app/chat/dm/[userId]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { MessageSkeleton } from "@/components/message-skeleton"
-
-export default function DMLoading() {
-  return (
-    <div className="flex-1 h-full">
-      <MessageSkeleton />
-    </div>
-  )
-} 
